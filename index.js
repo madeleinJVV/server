c160bdc7a5fa568700164fa78b088fba099d2623
Len Heard <tech@quickfeed.co.za>
2020-01-24 14:37:08 +0200

    test commit

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send ({bye:'Buddy'})
-});
+passport.use(new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback' //this is where the user will be redirected after they have given us the consent
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(refreshToken);
+        console.log(profile);
+    }
+    )
+);
 
-const PORT = process.env.PORT||5000;
-app.listen (PORT);
+app.get('/auth/google',
+    passport.authenticate('google', { //google was not specified - in google strategy it knows to use the google strategy
+        scope: ['profile', 'email'] //specifies what access we want from google - google has a list of all the scopes
+    })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google')
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
